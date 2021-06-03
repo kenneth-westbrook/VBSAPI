4070880340a0a2e05d4af8f37f7bea058a2f7917
Jason Westbrook <Kenneth.Westbrook@va.gov>
2021-06-03 08:27:49 -0400

    Initial creation of application

---
+++ VBSAPI/VBSAPI/Controllers/VBSAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AWSServerless1.Controllers
+{
+    [Route("vbsapi")]
+    [ApiController]
+    public class VBSAPIController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetStatementsByVeteranID")]
+        public IActionResult GetStatementsByVeteranID()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("GetStations")]
+        public IActionResult GetStations()
+        {
+            return Ok();
+        }
+    }
+}
